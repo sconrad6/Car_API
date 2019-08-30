b1bd00990450d44aee56f4debd6a0eaa62a856b1
Savanah Conrad <sibgym@yahoo.com>
2019-08-30 14:18:38 -0400

    First commit

---
+++ CarApi/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarApi
+{
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ CarApi/Models/CarContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarApi.Models
+{
+    public class CarContext : DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options)
+            : base (options)
+        {
+        }
+        public DbSet<Car> Car { get; set; }
+    }
+}
